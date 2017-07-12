d86fc499cd55aff353e83f17358c36e38e309354
Thierry Danis <tdanis@cisco.com>
2017-07-12 16:33:37 +0200

    scripts/StartAll-1.sh : new instructions to take into account the pvdd installation and pvdd node package

--- scripts/StartAll-1.sh
+++ scripts/StartAll-1.sh
@@ -6,19 +6,24 @@
 # This script is intended to be started on a host (at least,
 # it offers host [aka non router] functionality)
 
-# It supposes for now that source repositories have been cloned
-# besides this repository. This is not fine
+# It supposes that the pvdd component (daemon + its additional
+# utilities and binding package [the pvdd node one notably])
+# have been installed. By default, the installation has been
+# done under /usr/local
 
 # Make sure to run any sudo command before starting this script
 # (otherwise starting the pvdd will fail by waiting input
 # on stdin)
 
+
 cd ../pvdd || exit 1
 
-nohup node ./tests/httpsServer.js >/dev/null 2>&1 &
-nohup sudo /bin/sh -c 'while true; do ./src/obj/pvdd; sleep 1; done' >/dev/null 2>&1 &
-nohup node ./utils/pvdid-monitor -d >/dev/null 2>&1 &
+PATH=/usr/local/sbin:$PATH
+export PATH
 
+nohup node ./tests/httpsServer.js >/dev/null 2>&1 &
+nohup sudo pvdd >/dev/null 2>&1 &
+nohup node pvd-monitor -d >/dev/null 2>&1 &
 
 cd ../pvd-demo
-nohup node ./pvd-html-client/pvdHttpServer.js >/dev/null 2>&1 &
+NODE_PATH=/usr/local/lib/node_modules nohup node ./pvd-html-client/pvdHttpServer.js >/dev/null 2>&1 &
